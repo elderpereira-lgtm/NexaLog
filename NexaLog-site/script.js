5e0dcfcc1e3ca76327ecc9ccea902a1278e92fe7
Hiroshi <elder.pereira@aluno.senai.br>
2026-02-13 09:25:15 -0300

    Update script.js
    testes no js

--- NexaLog-site/script.js
+++ NexaLog-site/script.js
@@ -1,14 +1,23 @@
+// Loading inicial
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    document.getElementById("loading-screen").style.display = "none";
+    document.body.style.overflow = "auto";
+  }, 1500);
+});
+
+// Abrir / fechar menu
+function toggleMenu() {
+  document.getElementById("sidebar").classList.toggle("active");
+}
+
+// Navegação entre páginas
 function navigate(pageId) {
-
-  document.querySelectorAll('.page').forEach(page => {
-    page.classList.remove('active');
+  document.querySelectorAll(".page").forEach(page => {
+    page.classList.remove("active");
   });
 
-  document.querySelectorAll('.nav-item').forEach(btn => {
-    btn.classList.remove('active');
-  });
+  document.getElementById(pageId).classList.add("active");
 
-  document.getElementById(pageId).classList.add('active');
-
-  event.target.classList.add('active');
+  document.getElementById("sidebar").classList.remove("active");
 }
