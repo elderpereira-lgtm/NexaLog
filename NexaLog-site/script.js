9c6af05e34a142d701dfa790ade5a80965b7ece6
Hiroshi <elder.pereira@aluno.senai.br>
2026-02-13 09:58:52 -0300

    Update script.js
    alteração teste no js

--- NexaLog-site/script.js
+++ NexaLog-site/script.js
@@ -1,14 +1,50 @@
-function navigate(pageId) {
+let autenticado = false;
 
-  document.querySelectorAll('.page').forEach(page => {
-    page.classList.remove('active');
+function showCadastro(){
+  trocarTela("cadastroPage");
+}
+
+function voltarLogin(){
+  trocarTela("loginPage");
+}
+
+function verificacao(){
+  trocarTela("verificacaoPage");
+}
+
+function confirmarVerificacao(){
+  autenticado = true;
+  entrarSistema();
+}
+
+function login(){
+  autenticado = true;
+  entrarSistema();
+}
+
+function entrarSistema(){
+  trocarTela("loadingPage");
+
+  setTimeout(()=>{
+    document.getElementById("loadingPage").style.display="none";
+    document.getElementById("appPage").style.display="block";
+  },2000);
+}
+
+function trocarTela(id){
+  document.querySelectorAll("section").forEach(sec=>{
+    sec.style.display="none";
   });
+  document.getElementById(id).style.display="flex";
+}
 
-  document.querySelectorAll('.nav-item').forEach(btn => {
-    btn.classList.remove('active');
+function toggleMenu(){
+  document.getElementById("sidebar").classList.toggle("active");
+}
+
+function navigate(id){
+  document.querySelectorAll(".page").forEach(p=>{
+    p.classList.remove("active");
   });
-
-  document.getElementById(pageId).classList.add('active');
-
-  event.target.classList.add('active');
+  document.getElementById(id).classList.add("active");
 }
