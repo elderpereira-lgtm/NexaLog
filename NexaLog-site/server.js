d43c38c3ffb66365ce8b25d9f822a21ac6aa2b0d
Hiroshi <elder.pereira@aluno.senai.br>
2026-02-23 14:23:54 -0300

    Update server.js
    modificações no arquivo server.js

--- NexaLog-site/server.js
+++ NexaLog-site/server.js
@@ -7,14 +7,16 @@ const app = express();
 app.use(express.json());
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: "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",
 });
 
 app.post("/gerar-descricao", async (req, res) => {
   const { nomeProduto } = req.body;
 
-  const response = await openai.chat.completions.create({
-    model: "gpt-4o-mini",
+  const response = openai.responses.create({
+    model: "gpt-5-nano",
+    input: "write a haiku about ai",
+    store: true,
     messages: [
       {
         role: "user",
@@ -25,7 +27,7 @@ app.post("/gerar-descricao", async (req, res) => {
 
   res.json({ descricao: response.choices[0].message.content });
 });
-
+response.then((result) => console.log(result.output_text));
 app.listen(3000, () => {
   console.log("Servidor rodando na porta 3000");
 });
