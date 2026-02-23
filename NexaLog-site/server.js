1602349b94e776cbb85d577b0a0932f24ba213ca
Hiroshi <elder.pereira@aluno.senai.br>
2026-02-23 14:58:11 -0300

    Update server.js
    atualização da api key

--- NexaLog-site/server.js
+++ NexaLog-site/server.js
@@ -7,14 +7,14 @@ const app = express();
 app.use(express.json());
 
 const openai = new OpenAI({
-  apiKey: "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",
+  apiKey: "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",
 });
 
 app.post("/gerar-descricao", async (req, res) => {
   const { nomeProduto } = req.body;
 
   const response = openai.responses.create({
-    model: "gpt-5-nano",
+    model: "gpt-4.1",
     input: "write a haiku about ai",
     store: true,
     messages: [
@@ -27,7 +27,7 @@ app.post("/gerar-descricao", async (req, res) => {
 
   res.json({ descricao: response.choices[0].message.content });
 });
-response.then((result) => console.log(result.output_text));
-app.listen(3000, () => {
-  console.log("Servidor rodando na porta 3000");
+
+app.listen(5500, () => {
+  console.log("Servidor rodando na porta 5500");
 });
