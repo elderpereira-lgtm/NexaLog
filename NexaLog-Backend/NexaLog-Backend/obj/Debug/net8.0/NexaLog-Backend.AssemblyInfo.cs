7b0426922efc831490040fe14a5287f37f041a69
Hiroshi <elder.pereira@aluno.senai.br>
2026-06-03 16:42:04 -0300

    Correções

--- NexaLog-Backend/NexaLog-Backend/obj/Debug/net8.0/NexaLog-Backend.AssemblyInfo.cs
+++ NexaLog-Backend/NexaLog-Backend/obj/Debug/net8.0/NexaLog-Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NexaLog-Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90683106d4849613bae43ba441640f4e10887aff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+495bcdc646a4b0f535fe651cd51417e11765130a")]
 [assembly: System.Reflection.AssemblyProductAttribute("NexaLog-Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NexaLog-Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
